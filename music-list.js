5b4ab7641b5d3acacc81cdf9ecde33d5ecd11f1a
RAVI PRAKASH MISHRA <raviprakash.mishra2002@gmail.com>
2025-03-11 02:28:41 +0530

    updated

---
+++ music-list.js
@@ -0,0 +1,62 @@
+let allMusic = [
+    {
+        name: "Jhol",
+        artist: "Annural Khaild and Maanu",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Labon ko",
+        artist: "KK",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Soch",
+        artist: "Hardy Sandhu",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Darkside",
+        artist: "Alan Walker",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Love me like you do",
+        artist: "Ellie goulding",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Alone",
+        artist: "Alan Walker",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "hanuman chalisa",
+        artist: "Gulsan Kumar",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "Main Bola Parvat ka tu Rani Mahali",
+        artist: "Bolenath Mahadev",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "Salamat",
+        artist: "Ravi Prakash Mishra",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "",
+        artist: "",
+        img: "music-10",
+        src: "music-10"
+    },
+];
